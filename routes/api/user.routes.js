de5712ee248964f1244154b1d1c61c392b19d66f
Kaylee Stevens <kayls@Kaylees-MBP.lan>
2021-11-21 14:29:02 -0600

    working pw hashing

--- routes/api/user.routes.js
+++ routes/api/user.routes.js
@@ -50,6 +50,23 @@ router.post('/', (req, res) => {
         });
 });
 
+router.post('/login', (req, res) => {
+    User.findOne ({
+        where: {
+            email:req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address found' });
+            return;
+        }
+
+        res.json({ user: dbUserData });
+
+        // verify user 
+    });
+});
+
 //PUT /api/user/1
 router.put('/:id', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
